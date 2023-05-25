b5f12a40c22ea8a762c878157a878f41fa9f5388
Nico Ampoloquio <116412203+NicoAmpoloquio@users.noreply.github.com>
2023-05-25 12:01:16 +0800

    Initial commit

---
+++ AR.Data/StudentAttendanceRecords.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AR.Data
+{
+    public class StudentAttendanceRecords
+    {
+        public string StudentName { get; }
+        public List<AttendanceRecords> AttendanceRecords { get; }
+
+        public StudentAttendanceRecords(string studentName)
+        {
+            StudentName = studentName;
+            AttendanceRecords = new List<AttendanceRecords>();
+        }
+    }
+    public class AttendanceRecords
+    {
+        public DateTime Time { get; }
+        public string Status { get; }
+
+        public AttendanceRecords(DateTime time, string status)
+        {
+            Time = time;
+            Status = status;
+        }
+    }
+}
---
+++ AR.Models/AttendanceRecorder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AR.Models
+{
+    public class StudentAttendanceRecord
+    {
+        public string StudentName { get; }
+        public List<AttendanceRecord> AttendanceRecords { get; }
+
+        public StudentAttendanceRecord(string studentName)
+        {
+            StudentName = studentName;
+            AttendanceRecords = new List<AttendanceRecord>();
+        }
+    }
+
+    public class AttendanceRecord
+    {
+        public DateTime Time { get; }
+        public string Status { get; }
+
+        public AttendanceRecord(DateTime time, string status)
+        {
+            Time = time;
+            Status = status;
+        }
+    }
+}
