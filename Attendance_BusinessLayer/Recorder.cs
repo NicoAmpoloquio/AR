8b26322f41ca4015ae81060123a5dba2ec88c325
Nico Ampoloquio <116412203+NicoAmpoloquio@users.noreply.github.com>
2023-05-26 13:48:15 +0800

    Recreating n-tier layers, and categorizing each layers. As well as organizing some files.

---
+++ Attendance_BusinessLayer/Recorder.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using Attendance_DataLayer;
+using Attendance_Models;
+using Attendance_UserInterface;
+
+namespace Attendance_BusinessLayer
+{
+    public class AttendanceManager
+    {
+        public static void RecordAttendance()
+        {
+            string studentName = UserInterfaceRecord.GetStudentName();
+
+            StudentAttendanceRecord studentRecord = Students.StudentList.Find(record => record.StudentName == studentName);
+
+            if (studentRecord == null)
+            {
+                UserInterfaceRecord.DisplayInvalidStudentNameMessage();
+                return;
+            }
+
+            string statusChoice = UserInterfaceRecord.GetAttendanceStatus();
+            string attendanceStatus;
+
+            switch (statusChoice)
+            {
+                case "1":
+                    attendanceStatus = "Present";
+                    break;
+                case "2":
+                    attendanceStatus = "Absent";
+                    break;
+                case "3":
+                    attendanceStatus = "Excused";
+                    break;
+                default:
+                    UserInterfaceRecord.DisplayInvalidChoiceMessage();
+                    return;
+            }
+
+            DateTime currentTime = DateTime.Now;
+
+            studentRecord.StudentList.Add(new RecordDateTime(currentTime, attendanceStatus));
+            UserInterfaceRecord.DisplayAttendanceMarkedMessage();
+
+            //Console.Write("Enter student name: ");
+            //string studentName = Console.ReadLine();
+
+            //StudentAttendanceRecord studentRecord = Students.StudentList.Find(record => record.StudentName == studentName);
+
+            //if (studentRecord == null)
+            //{
+            //    Console.WriteLine("Invalid student name! Attendance not marked.");
+            //    return;
+            //}
+
+            //Console.WriteLine("Select attendance status:");
+            //Console.WriteLine("1. Present");
+            //Console.WriteLine("2. Absent");
+            //Console.WriteLine("3. Excused");
+            //Console.Write("Enter your choice: ");
+            //string statusChoice = Console.ReadLine();
+
+            //string attendanceStatus;
+
+            //switch (statusChoice)
+            //{
+            //    case "1":
+            //        attendanceStatus = "Present";
+            //        break;
+            //    case "2":
+            //        attendanceStatus = "Absent";
+            //        break;
+            //    case "3":
+            //        attendanceStatus = "Excused";
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid choice! Attendance not marked.");
+            //        return;
+            //}
+
+            //DateTime currentTime = DateTime.Now;
+
+            //studentRecord.StudentList.Add(new RecordDateTime(currentTime, attendanceStatus));
+            //Console.WriteLine("Attendance marked successfully.");
+        }
+        public static void ViewAttendanceRecordsByStudent()
+        {
+            string studentName = UserInterfaceRecord.GetStudentName();
+
+            StudentAttendanceRecord studentRecord = Students.StudentList.Find(record => record.StudentName == studentName);
+
+            if (studentRecord == null)
+            {
+                UserInterfaceView.DisplayNoRecordsFoundMessage();
+                return;
+            }
+
+            UserInterfaceView.DisplayAttendanceRecords(studentName, studentRecord.StudentList);
+            //Console.Write("Enter student name: ");
+            //string studentName = Console.ReadLine();
+
+            //StudentAttendanceRecord studentRecord = Students.StudentList.Find(record => record.StudentName == studentName);
+
+            //if (studentRecord == null)
+            //{
+            //    Console.WriteLine("No attendance records found for this student.");
+            //    return;
+            //}
+
+            //Console.WriteLine($"Attendance records for {studentName}:");
+            //foreach (var record in studentRecord.StudentList)
+            //{
+            //    Console.WriteLine($"Date: {record.Time.ToShortDateString()}, Time: {record.Time.ToShortTimeString()}, Status: {record.Status}");
+            //}
+        }
+        public static void ViewAttendanceRecordsForAllStudents()
+        {
+            //Console.WriteLine("Attendance records for all students:");
+
+            //if (studentList.Count == 0)
+            //{
+            //    Console.WriteLine("No attendance records found for any student.");
+            //    return;
+            //}
+
+            //foreach (var student in studentList)
+            //{
+            //    Console.WriteLine($"Student: {student.StudentName}");
+            //    foreach (var record in student.StudentList)
+            //    {
+            //        Console.WriteLine($"Date: {record.Time.ToShortDateString()}, Time: {record.Time.ToShortTimeString()}, Status: {record.Status}");
+            //    }
+            //    Console.WriteLine();
+            //}
+            Console.WriteLine("Attendance records for all students:");
+
+            if (Students.StudentList.Count == 0)
+            {
+                Console.WriteLine("No attendance records found for any student.");
+                return;
+            }
+
+            foreach (var student in Students.StudentList)
+            {
+                Console.WriteLine($"Student: {student.StudentName}");
+                foreach (var record in student.StudentList)
+                {
+                    Console.WriteLine($"Date: {record.Time.ToShortDateString()}, Time: {record.Time.ToShortTimeString()}, Status: {record.Status}");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void EditAttendance()
+        {
+            Console.Write("Enter student name: ");
+            string studentName = Console.ReadLine();
+
+            StudentAttendanceRecord studentRecord = Students.StudentList.Find(record => record.StudentName == studentName);
+
+            if (studentRecord == null)
+            {
+                Console.WriteLine("No attendance records found for this student.");
+                return;
+            }
+
+            Console.WriteLine($"Attendance records for {studentName}:");
+            for (int i = 0; i < studentRecord.StudentList.Count; i++)
+            {
+                var record = studentRecord.StudentList[i];
+                Console.WriteLine($"{i + 1}. Date: {record.Time.ToShortDateString()}, Time: {record.Time.ToShortTimeString()}, Status: {record.Status}");
+            }
+
+            Console.Write("Enter the number of the record you want to edit: ");
+            string recordChoice = Console.ReadLine();
+
+            if (!int.TryParse(recordChoice, out int recordIndex) || recordIndex < 1 || recordIndex > studentRecord.StudentList.Count)
+            {
+                Console.WriteLine("Invalid record number! Attendance not edited.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Select new attendance status:");
+                Console.WriteLine("1. Present");
+                Console.WriteLine("2. Absent");
+                Console.WriteLine("3. Excused");
+                Console.Write("Enter your choice: ");
+                string statusChoice = Console.ReadLine();
+
+                string attendanceStatus;
+
+                switch (statusChoice)
+                {
+                    case "1":
+                        attendanceStatus = "Present";
+                        break;
+                    case "2":
+                        attendanceStatus = "Absent";
+                        break;
+                    case "3":
+                        attendanceStatus = "Excused";
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Attendance not edited.");
+                        return;
+                }
+                DateTime currentTime = DateTime.Now;
+
+                studentRecord.StudentList.Add(new RecordDateTime(currentTime, attendanceStatus));
+            }
+
+            Console.WriteLine("Attendance edited successfully.");
+        }
+    }
+}
+
---
+++ Attendance_UserInterface/UserInterface.cs
@@ -0,0 +1,143 @@
+﻿using Attendance_DataLayer;
+using Attendance_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attendance_UserInterface
+{
+    public class UserInterfaceMenu
+    {
+        public static void DisplayMainTitle()
+        {
+            Console.WriteLine("Attendance is a Must, attendance muna bago umalis mga ya.");
+            Console.WriteLine("-------------------------------------------");
+        }
+        public static void DisplayMainMenu()
+        {
+            Console.WriteLine("\nPlease select an option:");
+            Console.WriteLine("1. Record attendance");
+            Console.WriteLine("2. View attendance records by student");
+            Console.WriteLine("3. View attendance records for all students");
+            Console.WriteLine("4. Edit attendance records");
+            Console.WriteLine("5. Exit");
+            Console.Write("Enter your choice: ");
+        }
+        public static void DisplayExitMessage()
+        {
+            Console.WriteLine("Bounce program na par...");
+        }
+
+        public static void DisplayInvalidChoiceMessage()
+        {
+            Console.WriteLine("Invalid choice, please try again.");
+        }
+    }
+    public class UserInterfaceRecord
+    {
+        public static string GetStudentName()
+        {
+            Console.Write("Enter student name: ");
+            return Console.ReadLine();
+        }
+        public static string GetAttendanceStatus()
+        {
+            Console.WriteLine("Select attendance status:");
+            Console.WriteLine("1. Present");
+            Console.WriteLine("2. Absent");
+            Console.WriteLine("3. Excused");
+            Console.Write("Enter your choice: ");
+            return Console.ReadLine();
+        }
+        public static void DisplayInvalidStudentNameMessage()
+        {
+            Console.WriteLine("Invalid student name! Attendance not marked.");
+        }
+        public static void DisplayInvalidChoiceMessage()
+        {
+            Console.WriteLine("Invalid choice! Attendance not marked.");
+        }
+        public static void DisplayAttendanceMarkedMessage()
+        {
+            Console.WriteLine("Attendance marked successfully.");
+        }
+        public static void DisplayPresent()
+        {
+            Console.WriteLine("Present");
+        }
+        public static void DisplayAbsent()
+        {
+            Console.WriteLine("Absent");
+        }
+        public static void DisplayExcused()
+        {
+            Console.WriteLine("Excused");
+        }
+        public static void AttendanceStatusChoices()
+        {
+
+            //Console.Write("Enter student name: ");
+            //string studentName = Console.ReadLine();
+
+            //StudentAttendanceRecord studentRecord = Students.StudentList.Find(record => record.StudentName == studentName);
+
+            //if (studentRecord == null)
+            //{
+            //    Console.WriteLine("Invalid student name! Attendance not marked.");
+            //    return;
+            //}
+
+            //Console.WriteLine("Select attendance status:");
+            //Console.WriteLine("1. Present");
+            //Console.WriteLine("2. Absent");
+            //Console.WriteLine("3. Excused");
+            //Console.Write("Enter your choice: ");
+            //string statusChoice = Console.ReadLine();
+
+            //string attendanceStatus;
+
+            //switch (statusChoice)
+            //{
+            //    case "1":
+            //        attendanceStatus = "Present";
+            //        break;
+            //    case "2":
+            //        attendanceStatus = "Absent";
+            //        break;
+            //    case "3":
+            //        attendanceStatus = "Excused";
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid choice! Attendance not marked.");
+            //        return;
+            //}
+
+            //DateTime currentTime = DateTime.Now;
+
+            //studentRecord.StudentList.Add(new RecordDateTime(currentTime, attendanceStatus));
+            //Console.WriteLine("Attendance marked successfully.");
+        }
+    }
+    public class UserInterfaceView
+    {
+        public static void DisplayNoRecordsFoundMessage()
+        {
+            Console.WriteLine("No attendance records found for this student.");
+        }
+
+        public static void DisplayAttendanceRecords(string studentName, List<RecordDateTime> records)
+        {
+            Console.WriteLine($"Attendance records for {studentName}:");
+            foreach (var record in records)
+            {
+                Console.WriteLine($"Date: {record.Time.ToShortDateString()}, Time: {record.Time.ToShortTimeString()}, Status: {record.Status}");
+            }
+        }
+    }
+    public class UserInterfaceViewAll
+    {
+
+    }
+}
